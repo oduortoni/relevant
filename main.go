6c7a610996c045aca312209cd4da288672a42a66
jamos <amosjoel91@gmail.com>
2024-10-14 16:21:02 +0300

    fix: fix route

--- main.go
+++ main.go
@@ -11,8 +11,8 @@ func main() {
 	http.HandleFunc("/", routes.Homepage)
 	http.HandleFunc("/gregister", routes.GRegister)
 	http.HandleFunc("/pregister", routes.PRegister)
-	// http.HandleFunc("/glogin", routes.GLogin)
-	// http.HandleFunc("/plogin", routes.PLogin)
+	http.HandleFunc("/glogin", routes.GLogin)
+	http.HandleFunc("/plogin", routes.PLogin)
 
 	fmt.Println("Server running on :9000")
 	http.ListenAndServe(":9000", nil)
